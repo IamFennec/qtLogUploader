5d175d28ee994940e094413ffb5f52bbb988580d
Matthew Read <mjr34d@gmail.com>
2023-11-21 19:28:37 +0100

    Upload log to dps.report

--- src/main/java/qtlog/DPSReportController/LogUploader.java
+++ src/main/java/qtlog/DPSReportController/LogUploader.java
@@ -3,9 +3,19 @@ package qtlog.DPSReportController;
 import qtlog.DataModel.ILogObserver;
 import qtlog.shared.LogDTO;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
-public class LogUploader implements ILogUploader, ILogObservable{
+public class LogUploader implements ILogUploader, ILogObservable {
     private ILogObserver observer;
     private LogDTO latestLog;
 
@@ -20,11 +30,28 @@ public class LogUploader implements ILogUploader, ILogObservable{
     }
 
     @Override
-    public void uploadLog(Path fileInfo) {
-        //logupload functionality here, maybe call helper functions etc.
-        //mock implementation
-        LogDTO tempLog = new LogDTO("www.snowcrows.com", 100, true, 100, "Deimos", false);
-        this.latestLog = tempLog;
+    public void uploadLog(Path logPath) {
+        try {
+            String boundary = Long.toHexString(System.currentTimeMillis());
+            File logFile = logPath.toFile();
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://dps.report/uploadContent?json=1&generator=ei"))
+                    .timeout(Duration.ofMinutes(15))
+                    .setHeader("Content-Type", "multipart/form-data; boundary=" + boundary)
+                    .POST(buildMultipartData(logFile, boundary))
+                    .build();
+
+            System.out.println("Uploading " + logPath.getFileName() + " to dps.report.");
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println("Response: " + response.statusCode());
+            // updateLatestLog(response.body());
+
+        } catch (IOException | InterruptedException | IllegalArgumentException e) {
+            e.printStackTrace();
+        }
         this.notifyObserver();
     }
 
@@ -32,7 +59,19 @@ public class LogUploader implements ILogUploader, ILogObservable{
     public LogDTO getLatestLogInfo() {
         return latestLog;
     }
-    
-    //create more private helper functions if needed
+
+    private static HttpRequest.BodyPublisher buildMultipartData(File file, String boundary) throws IOException {
+        List<byte[]> payload = new ArrayList<byte[]>();
+        payload.add(("--" + boundary + "\r\n" +
+                "Content-Disposition: form-data; name=\"file\"; filename=\"" + file.getName() + "\"\r\n" +
+                "Content-Type: application/octet-stream\r\n\r\n").getBytes());
+        payload.add(Files.readAllBytes(file.toPath()));
+        payload.add(("\r\n--" + boundary + "--\r\n").getBytes());
+        return HttpRequest.BodyPublishers.ofByteArrays(payload);
+    }
+
+    // private static void updateLatestLog(String responseJson) {
+    // this.latestLog = new LogDTO()
+    // }
 
 }
